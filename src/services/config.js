2e634545c653b3746242cbbfc8b30ac60cc73857
Marcio Junior <marciobarrosjr@gmail.com>
2021-07-21 19:04:19 -0300

    alteracoes porta api node

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    //baseURL: 'http://localhost:3000/api/'       
-    baseURL: 'http://localhost:3000/api'
+    //baseURL: 'http://localhost:8001/api/'       
+    baseURL: 'http://localhost:81/api'
 })
