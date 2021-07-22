760981115400432b2480dd73105332c0cef392f0
Marcio Junior <marciobarrosjr@gmail.com>
2021-07-22 00:50:29 -0300

    testando mudar ip publico api droplet

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({   
-    baseURL: 'http://localhost:8001/api'
+    //baseURL: 'http://localhost:8001/api'
+    baseURL: 'http://167.99.6.129:81/api'
 })
