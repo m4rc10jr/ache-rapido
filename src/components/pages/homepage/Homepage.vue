b011f01c61fb002a5b33e6a21d1677caffbd81a4
Marcio Junior <marciobarrosjr@gmail.com>
2021-07-23 01:31:10 -0300

    alteracoes homepage

--- src/components/pages/homepage/Homepage.vue
+++ src/components/pages/homepage/Homepage.vue
@@ -10,64 +10,68 @@
                     </nav>
                 </div>
             </div>
-            <div class="row">
-                <div id="cards-kpi" class="col-12">
-                    <div class="shadow-sm p-3 mb-5 bg-white rounded">
-                        <b> Parcelas em atraso (+7 dias após vencimento)</b>
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th scope="col">Cliente</th>
-                                <th scope="col">Contrato</th>
-                                <th scope="col">Parcela</th>
-                                <th scope="col">Data</th>
-                                <th scope="col">Valor</th>
-                                <th scope="col">Pago</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr v-for="parcela of parcelas_atrasos" :key="parcela.IdParcela">
-                                <td>{{parcela.NomeCliente}}</td>
-                                <td>{{parcela.CodContrato}}</td>
-                                <td>{{parcela.NumParcela}}</td>
-                                <td>{{parcela.DataPrevista}}</td>
-                                <td>{{parcela.ValorParcela}}</td>
-                                <td>{{parcela.TotalValorPago}}</td>
-                            </tr>
-                            </tbody>
-                        </table>
+            <b-tabs content-class="mt-3">
+                <b-tab title="Parcelas em atraso (+7 dias após vencimento)">
+                    <div class="row">
+                        <div id="cards-kpi" class="col-12">
+                            <div class="shadow-sm p-3 mb-5 bg-white rounded">
+                                <table class="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">Cliente</th>
+                                        <th scope="col">Contrato</th>
+                                        <th scope="col">Parcela</th>
+                                        <th scope="col">Data</th>
+                                        <th scope="col">Valor</th>
+                                        <th scope="col">Pago</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr v-for="parcela of parcelas_atrasos" :key="parcela.IdParcela">
+                                        <td>{{parcela.NomeCliente}}</td>
+                                        <td>{{parcela.CodContrato}}</td>
+                                        <td>{{parcela.NumParcela}}</td>
+                                        <td>{{parcela.DataPrevista}}</td>
+                                        <td>{{parcela.ValorParcela}}</td>
+                                        <td>{{parcela.TotalValorPago}}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div id="cards-kpi" class="col-12">
-                    <div class="shadow-sm p-3 mb-5 bg-white rounded">
-                        <b> Parcelas a vencer (próximos 15 dias) </b>
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th scope="col">Cliente</th>
-                                <th scope="col">Contrato</th>
-                                <th scope="col">Parcela</th>
-                                <th scope="col">Data</th>
-                                <th scope="col">Valor</th>
-                                <th scope="col">Pago</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr v-for="parcela of parcelas_proximos_vencimentos" :key="parcela.IdParcela">
-                                <td>{{parcela.NomeCliente}}</td>
-                                <td>{{parcela.CodContrato}}</td>
-                                <td>{{parcela.NumParcela}}</td>
-                                <td>{{parcela.DataPrevista}}</td>
-                                <td>{{parcela.ValorParcela}}</td>
-                                <td>{{parcela.TotalValorPago}}</td>
-                            </tr>
-                            </tbody>
-                        </table>
+                </b-tab>
+                <b-tab title="Parcelas a vencer (próximos 15 dias)">
+                    <div class="row">
+                        <div id="cards-kpi" class="col-12">
+                            <div class="shadow-sm p-3 mb-5 bg-white rounded">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Cliente</th>
+                                            <th scope="col">Contrato</th>
+                                            <th scope="col">Parcela</th>
+                                            <th scope="col">Data</th>
+                                            <th scope="col">Valor</th>
+                                            <th scope="col">Pago</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="parcela of parcelas_proximos_vencimentos" :key="parcela.IdParcela">
+                                            <td>{{parcela.NomeCliente}}</td>
+                                            <td>{{parcela.CodContrato}}</td>
+                                            <td>{{parcela.NumParcela}}</td>
+                                            <td>{{parcela.DataPrevista}}</td>
+                                            <td>{{parcela.ValorParcela}}</td>
+                                            <td>{{parcela.TotalValorPago}}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>   
+                </b-tab>
+            </b-tabs>   
         </div>
     </main>
 </template>
